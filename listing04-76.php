5c28d6cf0fe9872cb49e25da29716f8e332c80cf
ruslan-fedin <dbj7Psec>
2020-10-12 13:10:06 +0300

    04.76

--- listing04-76.php
+++ listing04-76.php
@@ -1,9 +1,34 @@
 <?php
 
 
-class Person {
+class Person
+
+{
+    private $writer;
 
 
+    public function __construct( PersonWriter $writer)
+    {
+        $this->writer = $writer;
+    }
+
+    public function __call(string $method, array $args)
+    {
+        if (method_exists($this->writer,$method)){
+            return $this->writer->$method($this);
+
+        }
+    }
+
+    public function getName():string
+    {
+       return "Иван";
+    }
+
+    public function getAge():int
+    {
+       return 44;
+    }
 
 
 }
